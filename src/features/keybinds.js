52d8e57eefd2b2d11b7ac9c1fb80fcf4391e2bb4
MorgenBinIchRich <tiktokgeheimlevi@gmail.com>
2026-03-08 14:06:34 +0100

    Remove keybind functionality from keybinds.js

--- src/features/keybinds.js
+++ src/features/keybinds.js
@@ -1,9 +1,3 @@
 import { Keybind } from "KeybindFix"
 
 let keybindCooldown = 0;
-
-new Keybind("//pos1", Keyboard.KEY_NONE, "HousingHelper").registerKeyPress(() => {
-    if (Date.now() - keybindCooldown < 500) return;
-    keybindCooldown = Date.now();
-    ChatLib.say("//pos1");
-});
